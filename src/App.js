c430be5256991f3b0eaca4f4f6ec50122f68be92
Niko Felger <niko.felger@gmail.com>
2021-09-15 09:18:36 +0200

    Connect to MetaMask

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+window.fabiansEth = "0xE3270777E2943b435C3d0C2A6B719564Bfd65026";
+
+async function connectMetaMask(setAddress) {
+  const address = (await window.ethereum.request({ method: 'eth_requestAccounts' }))[0];
+  setAddress(address);
+}
+
 function App() {
+  const [address, setAddress] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {address
+       ? `Willkommen ${address}`
+       : <button onClick={() => connectMetaMask(setAddress)}>Mit MetaMask verbinden</button>
+      }
     </div>
   );
 }
