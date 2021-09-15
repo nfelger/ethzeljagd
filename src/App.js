4f852b12e92f2856bec025c5d93a9beadee952a6
Niko Felger <niko.felger@gmail.com>
2021-09-15 09:23:06 +0200

    Factor out metamask component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, { useState } from 'react';
 import './App.css';
+import MetaMaskConnector from './MetaMaskConnector';
 
 window.fabiansEth = "0xE3270777E2943b435C3d0C2A6B719564Bfd65026";
 
-async function connectMetaMask(setAddress) {
-  const address = (await window.ethereum.request({ method: 'eth_requestAccounts' }))[0];
-  setAddress(address);
-}
 
 function App() {
   const [address, setAddress] = useState(false);
@@ -15,7 +12,7 @@ function App() {
     <div className="App">
       {address
        ? `Willkommen ${address}`
-       : <button onClick={() => connectMetaMask(setAddress)}>Mit MetaMask verbinden</button>
+       : <MetaMaskConnector onGetAddress={setAddress} />
       }
     </div>
   );
---
+++ src/MetaMaskConnector.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+async function connectMetaMask(onGetAddress) {
+  const address = (await window.ethereum.request({ method: 'eth_requestAccounts' }))[0];
+  onGetAddress(address);
+}
+
+export default function MetaMaskConnector({ onGetAddress }) {
+
+  return (
+    <button onClick={() => connectMetaMask(onGetAddress)}>Mit MetaMask verbinden</button>
+  )
+}
+
