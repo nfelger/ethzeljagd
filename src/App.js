a34bbc0c9aab5da39a881eb707d8ce052a3fa5ee
Niko Felger <niko.felger@gmail.com>
2021-09-15 09:27:05 +0200

    Verify user identity

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React, { useState } from 'react';
 import './App.css';
 import MetaMaskConnector from './MetaMaskConnector';
 
-window.fabiansEth = "0xE3270777E2943b435C3d0C2A6B719564Bfd65026";
+// TODO: change to real addr
+const fabiansEth = "0xE3270777E2943b435C3d0C2A6B719564Bfd65026".toLowerCase();
 
 
 function App() {
-  const [address, setAddress] = useState(false);
+  const [address, setAddress] = useState("");
+
+  const isFabian = address === fabiansEth;
 
   return (
     <div className="App">
       {address
-       ? `Willkommen ${address}`
+       ? (isFabian
+          ? `Willkommen, fabians.eth`
+          : `Nicht Fabian. Hau ab.`)
        : <MetaMaskConnector onGetAddress={setAddress} />
       }
     </div>
